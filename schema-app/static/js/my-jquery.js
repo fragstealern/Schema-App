3e57e6b09d43e409f168851621f3f75b688809e9
Philipdev95 <philipdev@icloud.com>
2016-10-20 14:16:48 +0200

    CSS

--- schema-app/static/js/my-jquery.js
+++ schema-app/static/js/my-jquery.js
@@ -14,3 +14,21 @@ $(document).ready(function () {
         $(".busdiv").fadeToggle(500);
     });
 });
+
+var tday=new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
+var tmonth=new Array("January","February","March","April","May","June","July","August","September","October","November","December");
+
+function GetClock(){
+var d=new Date();
+var nday=d.getDay(),nmonth=d.getMonth(),ndate=d.getDate();
+var nhour=d.getHours(),nmin=d.getMinutes(),nsec=d.getSeconds();
+if(nmin<=9) nmin="0"+nmin
+if(nsec<=9) nsec="0"+nsec;
+
+document.getElementById('timeDisplay').innerHTML=""+tday[nday]+", "+tmonth[nmonth]+" "+ndate+" "+nhour+":"+nmin+":"+nsec+"";
+}
+
+window.onload=function(){
+GetClock();
+setInterval(GetClock,1000);
+}
