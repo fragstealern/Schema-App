5a7563af5f7cf4564455dc8609527761ffff47a4
Philipdev95 <philipdev@icloud.com>
2016-10-26 11:02:20 +0200

    kåmitta lite först

--- schema-app/static/js/my-jquery.js
+++ schema-app/static/js/my-jquery.js
@@ -1,10 +1,16 @@
 $(document).ready(function () {
+    'use strict';
     $(".traindiv").fadeOut(100);
     $("#submit").click(function () {
         $(".traindiv").fadeToggle(1000);
+        $('html,body').animate({
+            scrollTop: $("#tiden").offset().top
+        },
+            'slow');
     });
 });
 $(document).ready(function () {
+    'use strict';
     $("#show-train").fadeIn(2000);
     $("#show-bus").fadeIn(2000);
     $("#show-train").click(function () {
@@ -15,20 +21,26 @@ $(document).ready(function () {
     });
 });
 
-var tday=new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
-var tmonth=new Array("January","February","March","April","May","June","July","August","September","October","November","December");
+var tday = new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
+var tmonth = new Array("January","February","March","April","May","June","July","August","September","October","November","December");
 
-function GetClock(){
-var d=new Date();
-var nday=d.getDay(),nmonth=d.getMonth(),ndate=d.getDate();
-var nhour=d.getHours(),nmin=d.getMinutes(),nsec=d.getSeconds();
-if(nmin<=9) nmin="0"+nmin
-if(nsec<=9) nsec="0"+nsec;
+function GetClock() {
+    'use strict';
+    var d = new Date(),
+        nday = d.getDay(),
+        nmonth = d.getMonth(),
+        ndate = d.getDate(),
+        nhour = d.getHours(),
+        nmin = d.getMinutes(),
+        nsec = d.getSeconds();
+    if (nmin <= 9) nmin = "0" + nmin
+    if (nsec <= 9) nsec = "0" + nsec;
 
-document.getElementById('timeDisplay').innerHTML=""+tday[nday]+", "+tmonth[nmonth]+" "+ndate+" "+nhour+":"+nmin+":"+nsec+"";
+    document.getElementById('timeDisplay').innerHTML = "" + tday[nday] + ", " + tmonth[nmonth] + " " + ndate + " " + nhour + ":" + nmin + ":" + nsec + "";
 }
 
-window.onload=function(){
-GetClock();
-setInterval(GetClock,1000);
-}
+window.onload = function () {
+    'use strict';
+    GetClock();
+    setInterval(GetClock, 1000);
+};
