977035576ede2d54382f1a4ceb4cfea9ce94d144
Philipdev95 <philipdev@icloud.com>
2016-10-26 12:54:47 +0200

    shananigans

--- schema-app/static/js/grayscale.js
+++ schema-app/static/js/grayscale.js
@@ -4,21 +4,9 @@
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-// jQuery to collapse the navbar on scroll
-function collapseNavbar() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
-}
-
-$(window).scroll(collapseNavbar);
-$(document).ready(collapseNavbar);
-
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
-    $('#submit').bind('click', function(event) {
+    $('a').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
--- schema-app/static/js/my-jquery.js
+++ schema-app/static/js/my-jquery.js
@@ -1,13 +1,5 @@
 $(document).ready(function () {
-    'use strict';
-    $(".traindiv").fadeOut(100);
-    $("#submit").click(function () {
-        $(".traindiv").fadeToggle(1000);
-        $('html,body').animate({
-            scrollTop: $("#tiden").offset().top
-        },
-            'slow');
-    });
+    $("#tiden").css({"position": "fixed", "top": "0"});
 });
 $(document).ready(function () {
     'use strict';
@@ -20,27 +12,3 @@ $(document).ready(function () {
         $(".busdiv").fadeToggle(500);
     });
 });
-
-var tday = new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
-var tmonth = new Array("January","February","March","April","May","June","July","August","September","October","November","December");
-
-function GetClock() {
-    'use strict';
-    var d = new Date(),
-        nday = d.getDay(),
-        nmonth = d.getMonth(),
-        ndate = d.getDate(),
-        nhour = d.getHours(),
-        nmin = d.getMinutes(),
-        nsec = d.getSeconds();
-    if (nmin <= 9) nmin = "0" + nmin
-    if (nsec <= 9) nsec = "0" + nsec;
-
-    document.getElementById('timeDisplay').innerHTML = "" + tday[nday] + ", " + tmonth[nmonth] + " " + ndate + " " + nhour + ":" + nmin + ":" + nsec + "";
-}
-
-window.onload = function () {
-    'use strict';
-    GetClock();
-    setInterval(GetClock, 1000);
-};
