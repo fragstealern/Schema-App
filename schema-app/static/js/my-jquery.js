6d3a143e0223c95f0406e40d5a0410cf44822052
Oscar Andersson <oscarandersson1337@gmail.com>
2016-10-26 14:58:13 +0200

    FUNKAR WOPIDIFAKKINGWHO

--- schema-app/static/js/my-jquery.js
+++ schema-app/static/js/my-jquery.js
@@ -1,13 +1,4 @@
-$(document).ready(function () {
-    $("#findTime").on('submit', function () {
-        $('html, body').animate({
-            scrollTop: $("#tiden").offset().top
-        }, 1000);
-        return false;
-    });
-});
-    
-$(document).ready(function () {
+  $(document).ready(function () {
     'use strict';
     $("#show-train").fadeIn(5000);
     $("#show-bus").fadeIn(5000);
